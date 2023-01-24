a6b9bdac7b9e141d2c91db20bdfe95cf3e65345d
toxicxoxo <106758630+toxicxoxo@users.noreply.github.com>
2023-01-24 15:07:16 +0530

    Update __main__.py

--- Telegram/__main__.py
+++ Telegram/__main__.py
@@ -164,7 +164,7 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
                             ),
                             InlineKeyboardButton(
                                 text=gs(chat.id, "source_btn"),
-                                url="https://github.com/ITZ-ZAID/Telegram",
+                                url="",
                             ),
                         ],
                         [
@@ -260,7 +260,7 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
                             ),
                             InlineKeyboardButton(
                                 text=gs(chat.id, "source_btn"),
-                                url="https://github.com/ITZ-ZAID/Telegram",
+                                url="",
                             ),
                         ],
                         [
@@ -668,7 +668,7 @@ def donate(update: Update, _: CallbackContext):
         context: CallbackContext -
     """
 
-    update.effective_message.reply_text("can Donate Via \n Razorpay 👉 https://pages.razorpay.com/GODFATHERDONATIONS \n PayPal 👉 https://www.paypal.com/paypalme/mrakki58 for more Contact in @Godfatherakki!  >_<")
+    update.effective_message.reply_text("can Donate Via \n Razorpay 👉 https://t.me/real_homies!  >_<")
 
 
 @zaidmsg(Filters.status_update.migrate)
